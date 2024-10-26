cc10bf49193db38c56d82ed3cdd84fd842394d0d
seven05 <ock1997@gmail.com>
2024-10-26 15:08:25 +0900

    27

---
+++ 27.remove-element.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+        return len(nums)
+# @lc code=end
+
